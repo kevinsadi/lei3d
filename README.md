e289722d0fe2c28c55ed9052cd0b4cef7a214ad8
Kevin <ksadi3@gatech.edu>
2022-12-20 20:35:31 -0500

    Render window glfw and update readme

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Kek3d is a graphics engine I'm developing using OpenGL and C++. It's express purpose is to be used to animate the process of Stable Diffusion on Terrain and to implement a interior decorating game.
 
+Kek3d has originally been created using Visual Studio 2022 on Windows. To compile, please use CMake to create a build for whatever system you are currently running on. 
+
 ## Requirements
 Graphics card that support OpenGL 4.6
 
---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <glad/glad.h> 
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "kek3d", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	// resize the openGL context if a user changes the window size
+
+	void framebuffer_size_callback(GLFWwindow * window, int width, int height);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
