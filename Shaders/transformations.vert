0ba31da26ebdcd841a9eb700256303a6aa0883f9
kevinsadi <kevinsadi@live.com>
2022-12-29 22:19:12 -0500

    3d support

--- Shaders/transformations.vert
+++ Shaders/transformations.vert
@@ -1,9 +1,7 @@
 #version 330 core
 layout (location = 0) in vec3 aPos; // read in position from buffer data
-layout (location = 1) in vec3 vertColor; // read in color from buffer data
-layout (location = 2) in vec2 aTexCoord;
+layout (location = 1) in vec2 aTexCoord; // read in texture coordinate from buffer data
 
-out vec3 color; // for output to frag shader
 out vec2 TexCoord;
 
 uniform mat4 model;
@@ -14,6 +12,5 @@ uniform mat4 proj;
 void main()
 {
     gl_Position = proj * view * model * vec4(aPos.x, aPos.y, aPos.z, 1.0);
-    color = aPos;
     TexCoord = aTexCoord;
 }
--- main.cpp
+++ main.cpp
@@ -76,17 +76,55 @@ int main() {
 	stbi_image_free(data); // no memory leaks here, no sir
 
 	// note: each vertex's data is taken from the VBO currently specified as the array buffer
-	float vertices[] = { 
-		// positions          // colors	       // uv texture coords
-		 0.7f,  0.7f, 0.0f,  1.0f, 0.0f, 0.0f,  1.0f, 1.0f,          // top right        0
-		-0.7f,  0.7f, 0.0f,  0.0f, 1.0f, 0.0f,  0.0f, 1.0f,          // top left         1
-		 0.7f, -0.7f, 0.0f,  0.0f, 0.0f, 1.0f,  1.0f, 0.0f,          // bottom right     2
-		-0.7f, -0.7f, 0.0f,  1.0f, 0.0f, 0.0f,  0.0f, 0.0f           // bottom left      3
+	float vertices[] = {
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
+	/*
 	unsigned int indices[] = {
 		2, 0, 1, // first triangles
 		3, 2, 1 // second triangles
 	};
+	*/
 
 	unsigned int VAO; // vertex array object (containerize VBO and EBO)
 	glGenVertexArrays(1, &VAO);
@@ -97,22 +135,21 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	/*
 	unsigned int EBO; // element buffer object (store indices)
 	glGenBuffers(1, &EBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), &indices, GL_STATIC_DRAW);
+	*/
 
 	// position vert attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	// color vert attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (void*)(3 * sizeof(GL_FLOAT)));
+	// texture uv vert attribute
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (void*)(3 * sizeof(GL_FLOAT)));
 	glEnableVertexAttribArray(1);
 
-	// texture vert attribute
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (void*)(6 * sizeof(GL_FLOAT)));
-	glEnableVertexAttribArray(2);
 	// since I have to keep looking it up, the properties on this method is:
 	// index, numcomponents per vert attribute, type, normalized, stride, offset from beginning
 
@@ -122,10 +159,12 @@ int main() {
 	glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
 	// transforms on object
-	model = glm::rotate(model, glm::radians(-50.0f), glm::vec3(1.0, 0.0, 0.0));
+	model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(1.0, 1.0, 0.0));
 
 	// move camera back
 	view = glm::translate(view, glm::vec3(0.f, 0.f, -3.0f));
+
+	glEnable(GL_DEPTH_TEST);
 
 	// *** render loop ***
 	while (!glfwWindowShouldClose(window))
@@ -135,10 +174,12 @@ int main() {
 
 		// rendering
 		glClearColor(0, 0, 1, 1);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		shader.use();
+
+		model = glm::rotate(model, glm::radians(1.0f), glm::vec3(1.0, 1.0, 0.0));
 
 		unsigned int modelLoc = glGetUniformLocation(shader.shaderProgramID, "model");
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
@@ -150,7 +191,7 @@ int main() {
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture);
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		glBindVertexArray(0); // unbind vertex array
 
