6dbfad986ad5be2da221905fd46c4b3c2df55c9e
kevinsadi <kevinsadi@live.com>
2023-01-03 11:20:29 -0500

    add multiple cubes to visualize camera

--- main.cpp
+++ main.cpp
@@ -8,10 +8,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-
 #include "Shader.hpp"
-
-// no reference types here let's gooooo
 
 // TODO: make header file for main when we refactor
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -62,13 +59,16 @@ float vertices[] = {
 	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
 
-/*
-unsigned int indices[] = {
-	2, 0, 1, // first triangles
-	3, 2, 1 // second triangles
+glm::vec3 cubePositions[] = {
+	glm::vec3( 0.0f, 0.0f, 0.0f),
+	glm::vec3(-1.0f, 0.0f,-3.0f),
+	glm::vec3( 0.0f, 4.0f, 0.0f),
+	glm::vec3(-5.0f,-0.0f, 1.0f),
+	glm::vec3( 1.0f, 3.0f, 6.0f),
+	glm::vec3(-0.0f, 7.0f,-0.0f),
+	glm::vec3(-4.0f, 4.0f, 4.0f),
+	glm::vec3( 1.0f, 3.0f, 5.0f)
 };
-*/
-
 
 int main() {
 	// *** initialize window ***
@@ -137,13 +137,6 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	/*
-	unsigned int EBO; // element buffer object (store indices)
-	glGenBuffers(1, &EBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), &indices, GL_STATIC_DRAW);
-	*/
-
 	// position vert attribute
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -168,27 +161,34 @@ int main() {
 		glClearColor(0, 0, 1, 1);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		shader.use();
+		// set texture
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glBindVertexArray(VAO);
 
-		// update camera transformations in shader
-		glm::mat4 model = glm::mat4(1.0f);
+		// shaders 
+		shader.use();
+		shader.setUniformMat4(projection, "proj");
+
 		
+		// camera system
 		const float radius = 10.0f;
 		float camX = cos(glfwGetTime()) * radius;
 		float camZ = sin(glfwGetTime()) * radius;
 		glm::mat4 view;
 		view = glm::lookAt(glm::vec3(camX, 0, camZ), glm::vec3(0, 0, 0), glm::vec3(0.0, 1.0, 0.0));
-
-		shader.setUniformMat4(model, "model");
 		shader.setUniformMat4(view, "view");
-		shader.setUniformMat4(projection, "proj");
 
-		// draw geometry and specify textures for said geometry
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture);
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		// draw geometry
+		for (unsigned int cube = 0; cube < sizeof(cubePositions); cube++)
+		{
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[cube]);
+			
+			shader.setUniformMat4(model, "model");
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		glBindVertexArray(0); // unbind vertex array
 
