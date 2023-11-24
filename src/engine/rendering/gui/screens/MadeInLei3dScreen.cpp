48c34c9092b3a2d864fd14d08d70fc660f169705
averylg <71529955+averylg@users.noreply.github.com>
2023-11-24 04:03:02 -0500

    Adds splash screen logo fade-in and fade-out state machine

--- src/engine/rendering/gui/screens/MadeInLei3dScreen.cpp
+++ src/engine/rendering/gui/screens/MadeInLei3dScreen.cpp
@@ -21,20 +21,42 @@ namespace lei3d
 		return tex.id();
 	}
 
+    // Copied from SplashScreen.cpp
+    int MadeInLei3dScreen::GetSplashTexId()
+	{
+		static Texture splash = Texture("./data/textures/splash.png");
+		return splash.id();
+	}
+
     void MadeInLei3dScreen::Init()
     {
         GuiScreen::Init();
 
+        alpha = 0.0;
+
+        fadeState = INCREASING;
+
         m_shouldHideHUD = true;
+
+        GuiRect* background = new GuiRect(
+            GuiComponent::Anchor::TOP_LEFT,
+            { GuiComponent::Space::NORMALIZED, { 0, 0 } },
+            { GuiComponent::Space::NORMALIZED, { 1, 1 } },
+            { 1, 1, 1, 1 },
+            GetSplashTexId()
+        );
 
         GuiRect* lei3dlogo = new GuiRect(
 			GuiComponent::Anchor::CENTER,
-			{ GuiComponent::Space::NORMALIZED, { 0, 0 } },
+			{ GuiComponent::Space::NORMALIZED, { 0, -0.2 } },
 			{ GuiComponent::Space::PIXELS, { 1725, 407 } },
-			{ 1, 1, 1, 1 },
+			{ 1, 1, 1, alpha },
 			GetLei3dTexture());
         lei3dlogo->m_alignCenter = true;
 
+        logo_id = lei3dlogo->GetId();
+
+        AddComponent(background);
         AddComponent(lei3dlogo);
 
         start = clock();
@@ -44,9 +66,49 @@ namespace lei3d
     {
         GuiScreen::Update(screenSize, mousePos);
 
-        if (clock() - start > 3 * CLOCKS_PER_SEC)
+        delete m_components[logo_id];
+
+        UpdateAlpha();
+
+        GuiRect* lei3dlogo = new GuiRect(
+			GuiComponent::Anchor::CENTER,
+			{ GuiComponent::Space::NORMALIZED, { 0, -0.2 } },
+			{ GuiComponent::Space::PIXELS, { 1725, 407 } },
+			{ 1, 1, 1, alpha },
+			GetLei3dTexture());
+        lei3dlogo->m_alignCenter = true;
+        
+        m_components[logo_id] = lei3dlogo;
+
+        if (clock() - start > 4 * CLOCKS_PER_SEC)
         {
             GuiManager::Instance().QueueNextScreen(new MainMenuScreen());
+        }
+    }
+
+    /**
+     * Make the fade-in and fade-out
+     */
+    void MadeInLei3dScreen::UpdateAlpha() {
+        switch (fadeState) {
+            case INCREASING:
+                if (alpha < 1) {
+                    alpha += 0.03;
+                } else {
+                    fadeState = STATIC;
+                }
+                break;
+            case STATIC:
+                // 1284 for delta_alpha = 0.02, 897 for 0.03
+                if ((4 * CLOCKS_PER_SEC) - (clock() - start) <= 897) { // 897 = number of clocks to get alpha >= 1
+                    fadeState = DECREASING;
+                }
+                break;
+            case DECREASING:
+                alpha -= 0.03;
+                break;
+            default:
+                break;
         }
     }
 
--- src/engine/rendering/gui/screens/MadeInLei3dScreen.hpp
+++ src/engine/rendering/gui/screens/MadeInLei3dScreen.hpp
@@ -2,13 +2,27 @@
 #include "GuiScreen.hpp"
 #include <time.h>
 
+
 namespace lei3d 
 {
     class MadeInLei3dScreen : public GuiScreen
     {
     private:
 		static int GetLei3dTexture();
+        static int GetSplashTexId();
         clock_t start;
+        int logo_id;
+        double alpha;
+
+        void UpdateAlpha();
+
+        enum FadeState {
+            INCREASING,
+            STATIC,
+            DECREASING
+        };
+
+        FadeState fadeState;
 
     protected:
         void Init() override;
